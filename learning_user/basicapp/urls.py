4743f33e7e6e0aed01d1374c5ba93f0ea551e69b
sowmya <sowmi23@gmail.com>
2021-02-16 11:08:32 +0530

    first commit

---
+++ learning_user/basicapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from basicapp import views
+
+#namesapce
+app_name="basicapp"
+
+urlpatterns=[
+    path('register/',views.register,name="register"),
+    path('user_login/',views.user_login,name="user_login")
+]
